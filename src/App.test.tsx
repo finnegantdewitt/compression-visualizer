fd0567316590316b98390ee35dd71ef9fffe251e
finnegantdewitt <finn@finnegandewitt.com>
2022-04-09 21:10:24 -0700

    pretty much set up the project to get started working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders lyrics", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/We're no strangers to love/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="react-scripts" />
+// need this file for importing certain file types
+// https://stackoverflow.com/questions/67262914/what-is-the-react-app-env-d-ts-in-a-react-typescript-project-for
