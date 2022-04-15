4674eb6f64953bdf5f9ebbcf193c8b93f33fd52a
Austin Chen <aubchen@ucsc.edu>
2022-04-15 11:27:44 -0700

    initial file-upload branch commit

---
+++ src/components/file-upload.tsx
@@ -0,0 +1,15 @@
+// component that deals with txt file upload and processing
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+export default class App extends React.Component<IAppProps> {
+  public render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
