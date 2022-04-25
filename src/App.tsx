fceeff04fd566671e459ae23561d6819e425c591
finnegantdewitt <finn@finnegandewitt.com>
2022-04-25 14:30:22 -0700

    Finalizing the file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Never_Gonna_Lyrics from "./text/Never_Gonna";
-import GetFile from "./components/showFile"
-
+import GetFile from "./components/showFile";
 
 function App() {
-
+  const [fileText, setFileText] = useState<string | ArrayBuffer | null>(null);
 
   return (
-   <div className = "App">
-    <GetFile />
-   </div>
+    <div className="App">
+      <GetFile setFileText={setFileText} />
+      <div>{fileText}</div>
+    </div>
   );
 }
 
--- src/components/showFile.tsx
+++ src/components/showFile.tsx
@@ -1,39 +1,42 @@
 import React, { useState } from "react";
 
-
-
 // testing file input
-function GetFile() {
-  const [state, setState] = useState<string | ArrayBuffer | null>(null);
+function GetFile({
+  setFileText,
+}: {
+  setFileText: React.Dispatch<
+    React.SetStateAction<string | ArrayBuffer | null>
+  >;
+}) {
+  const [localFileText, setLocalFileText] = useState<
+    string | ArrayBuffer | null
+  >(null);
 
-  const changeHandler = (e:any) => {
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const reader = new FileReader();
     reader.onload = (e) => {
       const text = e.target!.result;
-      setState(text);
+      setLocalFileText(text);
     };
-    reader.readAsText(e.target.files[0]);
+    reader.readAsText(e.target.files![0]);
   };
 
   const submitHandler = () => {
-    if (state == null){
+    if (localFileText == null) {
       alert("Please select a .txt file.");
     } else {
-      console.log(state);
+      console.log(localFileText);
+      setFileText(localFileText);
     }
-  }
+  };
 
-  return(
+  return (
     <div className="FileSubmit">
-      <input type="file"  accept=".txt" onChange={changeHandler} />
-      <button onClick={submitHandler}>
-        Submit
-      </button>
+      <input type="file" accept=".txt" onChange={(e) => changeHandler(e)} />
+      <button onClick={submitHandler}>Submit</button>
     </div>
   );
 }
 
-
-
-export default GetFile
+export default GetFile;
