e31576042fab4488cc2687c0c61c08063c09c48d
Finnegan Dewitt <35671001+finnegantdewitt@users.noreply.github.com>
2022-04-27 20:16:07 -0700

    changed background and added a more simple sample text

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import './App.css';
 import LyricSplit from './components/LyricSplit';
 import Never_Gonna_Lyrics from './text/Never_Gonna';
-import GetFile from "./components/showFile";
+import GetFile from './components/showFile';
+import Simple from './text/Simple_Test_Text';
 
 function App() {
   const [fileText, setFileText] = useState<string | ArrayBuffer | null>(null);
   return (
     <div className="App">
-      <LyricSplit lyrics={Never_Gonna_Lyrics} />
       <header className="App-header">
+        <LyricSplit lyrics={Simple} />
         <GetFile setFileText={setFileText} />
         <div>{fileText}</div>
       </header>
