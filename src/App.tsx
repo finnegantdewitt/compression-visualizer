5c8d2fcfc48d86814fe170ab48b99ff72ce69ae6
Finnegan Dewitt <35671001+finnegantdewitt@users.noreply.github.com>
2022-05-18 21:50:30 -0700

    basic clock spolls out charecters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import LyricSplit from './components/LyricSplit';
 import Never_Gonna_Lyrics from './text/Never_Gonna';
@@ -7,12 +7,38 @@ import Simple from './text/Simple_Test_Text';
 import { Tree, createTree } from './components/Tree';
 
 function App() {
-  const [fileText, setFileText] = useState<string | ArrayBuffer | null>(null);
   let tree = createTree(Simple);
+  const [fileText, setFileText] = useState<string | ArrayBuffer | null>(null);
+  const [clock, setClock] = useState(0);
+  const [play, setPlay] = useState(false);
+  const [displayString, setDisplayString] = useState('');
+
+  useEffect(() => {
+    if (clock !== -1 && play) {
+      const interval = setInterval(() => {
+        onClock();
+      }, 50);
+      return () => clearInterval(interval);
+    }
+  }, [clock, play, displayString]);
+
+  function onClock() {
+    if (displayString.length < Simple.length) {
+      setDisplayString(displayString + Simple[displayString.length]);
+      console.log(displayString);
+    }
+  }
+
+  function pressPlay() {
+    setPlay(!play);
+    onClock();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <LyricSplit lyrics={Simple} />
+        <button onClick={() => pressPlay()}>play</button>
+        <LyricSplit lyrics={displayString} />
         <GetFile setFileText={setFileText} />
         <Tree treeData={tree} />
         <div>{fileText?.toString()}</div>
